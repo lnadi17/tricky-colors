20e02d23090e1d0cc82f44031699d63b403e9f76
Luka Nadiradze <thrilled.space@gmail.com>
2016-08-03 02:03:42 +0400

    Generate platforms
    Now it generates randomly colored platforms.

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Generator : MonoBehaviour {
+
+	public List<GameObject> colorHolders;
+	public GameObject colorHolder;
+
+	void Start () {
+		GenerateY (5);
+	}
+	
+	void Update () {
+		
+	}
+
+
+	//Generates lines randomly, but it'd be great if it didn't create 2 rects next to each other with the same color.
+	void GenerateX(float positionOffsetY, int times = 1){
+		float positionOffsetX = 0 - (colorHolders.Count * 1.5f * 0.5f * times) + 0.75f; //It's always in the middle of the screen.
+		GameObject colorHolderInstance = Instantiate(colorHolder, new Vector2(positionOffsetX, positionOffsetY), Quaternion.identity) as GameObject;
+		colorHolderInstance.AddComponent<PlatformTransform> ();
+		for (int u = 0; u < times; u++) {
+			for (int i = 0; i < colorHolders.Count; i++) {
+				int randomIndex = Random.Range (0, colorHolders.Count);
+				GameObject toInstantiate = colorHolders [randomIndex];
+				GameObject instance = Instantiate (toInstantiate, new Vector2 (positionOffsetX, positionOffsetY), Quaternion.identity) as GameObject;
+				instance.transform.SetParent (colorHolderInstance.transform);
+				positionOffsetX += 1.5f;
+			}
+		}
+	}
+
+	//Generates lines on Y axis. Default is 5 times.
+	void GenerateY(int times = 5){
+		float positionOffsetY = 0;
+		for (int i = 0; i < times; i++) {
+			GenerateX (positionOffsetY, 2);
+			positionOffsetY += 3;
+		}
+	}
+}

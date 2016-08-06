286e665582fa6ce81298255570925b21238d2faf
Luka Nadiradze <thrilled.space@gmail.com>
2016-08-06 11:09:55 +0400

    Refined UI
    Added fonts and some new sprites. Speed is slightly increased.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,37 @@ public class PlayerMovement : MonoBehaviour {
 	public Vector3 endDistance;
 	[Range(1, 10)]public float transpeed;
 	private Rigidbody2D rb2d;
+	private bool started;
+	private CircleCollider2D coll;
 
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
+		coll = GetComponent<CircleCollider2D> ();
+		transform.localScale = new Vector2 (25, 25);
+		coll.enabled = false;
+		started = false;
+		StartCoroutine (Shrink ());
 	}
 
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space) || Input.GetTouch(0).phase == TouchPhase.Began){
+		if (Input.GetTouch(0).phase == TouchPhase.Began && started){
 			if(rb2d.isKinematic){
 				rb2d.isKinematic = false;
 			}
 			Vector3 end = endDistance + (Vector3)rb2d.position;
 			StartCoroutine (SmoothMovement (end));
+		}
+	}
+
+	IEnumerator Shrink(){
+		while (started == false){
+			transform.localScale = new Vector2 (transform.localScale.x - 0.5f, transform.localScale.y - 0.5f);
+			if (transform.localScale.x <= 0.5f) {
+				transform.localScale = new Vector2 (0.5f, 0.5f);
+				coll.enabled = true;
+				started = true;
+			}
+			yield return new WaitForSeconds (.025f);
 		}
 	}
 
